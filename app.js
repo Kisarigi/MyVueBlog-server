50d79c70128cdba5ecb97c981549d6d00d8ea4b8
Kisarigi <9522434482@qq.com>
2018-03-18 17:06:05 +0800

    new

--- app.js
+++ app.js
@@ -28,7 +28,13 @@ app.use('/users', users);
 app.use('/article', article);
 app.use('/message', message);
 
-
+app.all('/', function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header('Access-Control-Allow-Methods', 'PUT, GET, POST, DELETE, OPTIONS');
+    res.header("Access-Control-Allow-Headers", "X-Requested-With");
+    res.header('Access-Control-Allow-Headers', 'Content-Type');
+    next();
+});
 
 
 // catch 404 and forward to error handler
